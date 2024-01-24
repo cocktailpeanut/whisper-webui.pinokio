40cff6f72f4b24559256f9386a36faaa778f6859
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-24 14:32:04 -0500

    update

--- pinokio.js
+++ pinokio.js
@@ -4,25 +4,44 @@ module.exports = {
   description: "A Web UI for easy subtitle using whisper model (https://github.com/jhj0517/Whisper-WebUI)",
   menu: async (kernel) => {
     let installed = await kernel.exists(__dirname, "app", "venv")
-    if (installed) {
+    let installing = await kernel.running(__dirname, "install.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
       let session = await kernel.require(__dirname, "session.json")
       let running = await kernel.running(__dirname, "start.json")
       if (running) {
         return [{
-          icon: "fa-solid fa-spin fa-circle-notch",
-          text: "Running"
-        }, {
           icon: "fa-solid fa-rocket",
           text: "Open UI",
           href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
           target: "_blank"
+        }, {
+          icon: 'fa-solid fa-terminal',
+          text: "Terminal",
+          href: "start.js",
         }]
       } else {
         return [{
           icon: "fa-solid fa-power-off",
-          text: "Launch",
-          href: "start.json",
-          params: { fullscreen: true, run: true }
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-solid fa-broom",
+          text: "Factory Reset",
+          href: "reset.js",
         }]
       }
     } else {
